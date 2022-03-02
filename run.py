1ee91191d459bcd7fc78f2f8877fb89101fb5e4d
Matthew Drury <matthew.drury.83@gmail.com>
2022-03-02 14:37:01 -0800

    Execute notebooks from run.py.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import json
+import subprocess
 
 template = open('template.ipynb', 'r').read()
 config = json.load(open('config.json', 'r'))
@@ -9,5 +10,11 @@ for c in config:
             .replace("[OFFSET]", str(c["OFFSET"]))
             .replace("[EXPONENT]", str(c["EXPONENT"]))
     )
+    filenm = f"notebooks/plot:{c['OFFSET']}:{c['EXPONENT']}.ipynb"
     with open(f"notebooks/plot:{c['OFFSET']}:{c['EXPONENT']}.ipynb", 'w') as f:
         f.write(notebook)
+    _ = subprocess.run(
+        # jupyter nbconvert --to notebook --inplace --execute filled-in-template.ipynb
+        ["jupyter", "nbconvert", "--to", "notebook", "--inplace", "--execute", filenm],
+        stdout=subprocess.PIPE
+    )
